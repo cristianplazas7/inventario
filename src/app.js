198463a208c6ec9bf9ddf13dba43a90819946094
cristianplazas7 <cristianplazasgarcia821@gmail.com>
2025-11-17 22:45:11 -0500

    enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import categoriasRoutes from "./routes/categorias.routes.js";
 
 const app= express();
 
 app.set("port",5000)
+
+app.use("/api/categorias",categoriasRoutes)
 
 export default app;
